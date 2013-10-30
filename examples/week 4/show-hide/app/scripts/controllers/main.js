5e88139f9962f474c8cadbaebfe40fb63dc8384e
Ben Jacobs <ben.jacobs@buildium.com>
2013-10-29 20:58:52 -0700

    updated example

--- examples/week 4/show-hide/app/scripts/controllers/main.js
+++ examples/week 4/show-hide/app/scripts/controllers/main.js
@@ -8,18 +8,31 @@ angular.module('showHideApp')
 	'Karma'
 	];
 
+	$scope.applyVar = '';
+
 	$scope.visibility = true;
 
 	$scope.effect = 'bounce';
+
 	$scope.completeShowFn = function () {
+		$scope.applyVar = 'show show show';
+
 		console.log('controller says: done showing');
 	};
 
 	$scope.completeHideFn = function () {
+		$scope.applyVar = 'hide hide hide';
+		
 		console.log('controller says: done hiding');
 	};
+
+	$scope.$watch('applyVar', function (newVal, oldVal) {
+		if(newVal) {
+			console.log(newVal);
+		}
+	})
 })
-.directive('animatedShow', function () {
+.directive('animatedShow', function ($rootScope) {
 	return {
 		scope: {
 			animatedShow: '=',
@@ -32,12 +45,36 @@ angular.module('showHideApp')
 				if (newVal) {
 					element.show({
 						effect: scope.effect,
-						complete: scope.completeShow
+						complete: function () {
+							// This SO post has information about dealing with
+							// the $digest in progress error.
+							// (http://stackoverflow.com/questions/12729122
+							// /prevent-error-digest-already-in-progress-when-
+							// calling-scope-apply) I noticed this was only
+							// happening on the first iteration, and all
+							// following clicks to the toggle service went off
+							// without a hitch. The SO post also links to some
+							// comments on the Angular JS github page that
+							// suggests that this kind of checking (to see if
+							// a digest is in progress) is NOT a good
+							// practice, in general. I think it's defensible
+							// here.
+
+							if (!($rootScope.$$phase || $rootScope.$apply())) {
+								scope.$apply(function () {
+									scope.completeShow();
+								})
+							}
+						}
 					});
 				} else {
 					element.hide({
 						effect: scope.effect,
-						complete: scope.completeHide
+						complete: function () {
+							scope.$apply(function () {
+								scope.completeHide();
+							});
+						}
 					});
 				}
 			});
